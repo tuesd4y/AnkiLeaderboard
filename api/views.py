70e7f63b1b88be167c7c02937ea4c6b5e6dbfe41
Eva Maria Mayer <49530340+eva-fitzinger@users.noreply.github.com>
2025-03-16 15:04:30 +0100

    Adds Views for Website

--- api/views.py
+++ api/views.py
@@ -1,236 +1,123 @@
+from django.contrib.auth.models import User
+from django.core.handlers.wsgi import WSGIRequest
+from django.http import Http404
 from django.shortcuts import render
 import sqlite3
 from datetime import datetime, timedelta
 import json
 
 from api.config import get_db_path
+from api.models import Leaderboard, UserProfile, League
 
 database_path = get_db_path()
 
 
+def generate_leaderboard_row(counter, user, value):
+    return {"place": counter, "username": user, "value": value}
+
+
 def reviews(request):
     data = []
-    counter = 1
-    start_day = datetime.now() - timedelta(days=1)
-    conn = sqlite3.connect(database_path)
-    c = conn.cursor()
-    c.execute(
-        "SELECT Username, Cards, Sync_Date FROM Leaderboard WHERE suspended IS NULL ORDER BY Cards DESC"
-    )
 
-    for row in c.fetchall():
-        sync_date = row[2]
-        sync_date = datetime.strptime(sync_date, "%Y-%m-%d %H:%M:%S.%f")
-
-        if sync_date > start_day:
-            x = {"place": counter, "username": row[0], "value": row[1]}
-            data.append(x)
-            counter += 1
+    for counter, leaderboard in enumerate(Leaderboard.objects.all().order_by("-cards_today"), start=1):
+        data.append(generate_leaderboard_row(counter, leaderboard.user, leaderboard.cards_today))
     return render(request, "reviews.html", {"data": data})
 
 
 def time(request):
     data = []
-    counter = 1
-    start_day = datetime.now() - timedelta(days=1)
-    conn = sqlite3.connect(database_path)
-    c = conn.cursor()
-    c.execute(
-        "SELECT Username, Time_Spend, Sync_Date FROM Leaderboard WHERE suspended IS NULL ORDER BY Time_Spend DESC"
-    )
 
-    for row in c.fetchall():
-        sync_date = row[2]
-        sync_date = datetime.strptime(sync_date, "%Y-%m-%d %H:%M:%S.%f")
-
-        if sync_date > start_day:
-            x = {"place": counter, "username": row[0], "value": row[1]}
-            data.append(x)
-            counter += 1
+    for counter, leaderboard in enumerate(Leaderboard.objects.all().order_by("-time_today"), start=1):
+        data.append(generate_leaderboard_row(counter, leaderboard.user, leaderboard.time_today))
     return render(request, "time.html", {"data": data})
 
 
 def streak(request):
     data = []
-    counter = 1
-    start_day = datetime.now() - timedelta(days=1)
-    conn = sqlite3.connect(database_path)
-    c = conn.cursor()
-    c.execute(
-        "SELECT Username, Streak, Sync_Date FROM Leaderboard WHERE suspended IS NULL ORDER BY Streak DESC"
-    )
-
-    for row in c.fetchall():
-        sync_date = row[2]
-        sync_date = datetime.strptime(sync_date, "%Y-%m-%d %H:%M:%S.%f")
-
-        if sync_date > start_day:
-            x = {"place": counter, "username": row[0], "value": row[1]}
-            data.append(x)
-            counter += 1
+    for counter, leaderboard in enumerate(Leaderboard.objects.all().order_by("-streak"), start=1):
+        data.append(generate_leaderboard_row(counter, leaderboard.user, leaderboard.streak))
     return render(request, "streak.html", {"data": data})
 
 
 def retention(request):
     data = []
-    counter = 1
-    start_day = datetime.now() - timedelta(days=1)
-    conn = sqlite3.connect(database_path)
-    c = conn.cursor()
-    c.execute(
-        "SELECT Username, Retention, Sync_Date FROM Leaderboard WHERE suspended IS NULL ORDER BY Retention DESC"
-    )
-
-    for row in c.fetchall():
-        sync_date = row[2]
-        sync_date = datetime.strptime(sync_date, "%Y-%m-%d %H:%M:%S.%f")
-
-        if sync_date > start_day and row[1] != "N/A" and row[1] != "":
-            x = {"place": counter, "username": row[0], "value": row[1]}
-            data.append(x)
-            counter += 1
+    for counter, leaderboard in enumerate(Leaderboard.objects.all().order_by("-retention"), start=1):
+        data.append(generate_leaderboard_row(counter, leaderboard.user, leaderboard.retention))
     return render(request, "retention.html", {"data": data})
 
 
 def user(request, username):
-    conn = sqlite3.connect(database_path)
-    c = conn.cursor()
-    user_data = c.execute(
-        "SELECT * FROM Leaderboard WHERE Username = (?)", (username,)
-    ).fetchone()
-    league = c.execute(
-        "SELECT league, history FROM League WHERE Username = (?)", (username,)
-    ).fetchone()
-    if not league:
-        league = ["None", "None"]
-    if user_data[7] == "Country" or "":
+    if not User.objects.filter(username=username).exists():
+        raise Http404("User does not exist")
+
+    auth_user = User.objects.get(username=username)
+
+    if not UserProfile.objects.filter(user=auth_user).exists():
+        raise Http404("User does not exist")
+
+    user_data = UserProfile.objects.get(user=auth_user)
+    leaderboard = None
+    country = user_data.country
+    groups = ["-"]
+
+    if Leaderboard.objects.filter(user=user_data.user).exists():
+        leaderboard = Leaderboard.objects.get(user=user_data.user)
+    if user_data.country == "Country" or "":
         country = "-"
+    if user_data.groups:
+        groups = user_data.groups
+
+    if leaderboard is None:
+        data = [{"username": username,
+                 "cards": 0,
+                 "streak": 0,
+                 "time": 0,
+                 "retention": 0,
+                 "month": 0,
+                 "country": country,
+                 "subject": ', '.join(groups),
+                 "league": user_data.league}]
     else:
-        country = user_data[7]
-    if user_data[12]:
-        groups = json.loads(user_data[12])
-    else:
-        groups = []
-    if user_data[6] == "Custom" or user_data[6] == "" or user_data[6] == None:
-        groups = ["-"]
-    else:
-        subject = user_data[6]
-        if subject not in [group.replace(" ", "") for group in groups]:
-            groups.append(subject)
-    data = [
-        {
-            "username": username,
-            "streak": user_data[1],
-            "cards": user_data[2],
-            "time": user_data[3],
-            "month": user_data[5],
-            "subject": ", ".join(groups),
-            "country": country,
-            "retention": user_data[8],
-            "league": league[0],
-        }
-    ]
+        data = [{"username": username,
+                 "cards": leaderboard.cards_today,
+                 "streak": leaderboard.streak,
+                 "time": leaderboard.time_today,
+                 "retention": leaderboard.retention,
+                 "month": leaderboard.cards_month,
+                 "country": country,
+                 "subject": ', '.join(groups),
+                 "league": user_data.league}]
     return render(request, "user.html", {"data": data})
 
 
-def alpha(request):
+def render_league(league_name: str, request: WSGIRequest):
     data = []
-    counter = 1
-    conn = sqlite3.connect(database_path)
-    c = conn.cursor()
-    c.execute(
-        "SELECT username, xp, time_spend, reviews, retention, league, days_learned FROM League WHERE suspended IS NULL ORDER BY xp DESC"
-    )
-
-    for row in c.fetchall():
-        if row[5] == "Alpha" and row[1] != 0:
-            x = {
-                "place": counter,
-                "username": row[0],
-                "xp": row[1],
-                "time": row[2],
-                "reviews": row[3],
-                "retention": row[4],
-                "days_learned": row[6],
-            }
-            data.append(x)
-            counter += 1
+    for counter, league in enumerate(League.objects.all().order_by("-xp"), start=1):
+        if UserProfile.objects.filter(user=league.user).exists():
+            user_profile = UserProfile.objects.get(user=league.user)
+            if user_profile.league == league_name and league.xp != 0:
+                data.append({"place": counter,
+                             "username": user_profile.user.username,
+                             "xp": league.xp,
+                             "time": league.time_spent,
+                             "reviews": league.cards,
+                             "retention": league.retention,
+                             "days_learned": league.days_studied, })
     return render(request, "leagues.html", {"data": data})
 
+
+def alpha(request):
+    return render_league("Alpha", request)
 
 def beta(request):
-    data = []
-    counter = 1
-    conn = sqlite3.connect(database_path)
-    c = conn.cursor()
-    c.execute(
-        "SELECT username, xp, time_spend, reviews, retention, league, days_learned FROM League WHERE suspended IS NULL ORDER BY xp DESC"
-    )
-
-    for row in c.fetchall():
-        if row[5] == "Beta" and row[1] != 0:
-            x = {
-                "place": counter,
-                "username": row[0],
-                "xp": row[1],
-                "time": row[2],
-                "reviews": row[3],
-                "retention": row[4],
-                "days_learned": row[6],
-            }
-            data.append(x)
-            counter += 1
-    return render(request, "leagues.html", {"data": data})
-
+    return render_league("Beta", request)
 
 def gamma(request):
-    data = []
-    counter = 1
-    conn = sqlite3.connect(database_path)
-    c = conn.cursor()
-    c.execute(
-        "SELECT username, xp, time_spend, reviews, retention, league, days_learned FROM League WHERE suspended IS NULL ORDER BY xp DESC"
-    )
-
-    for row in c.fetchall():
-        if row[5] == "Gamma" and row[1] != 0:
-            x = {
-                "place": counter,
-                "username": row[0],
-                "xp": row[1],
-                "time": row[2],
-                "reviews": row[3],
-                "retention": row[4],
-                "days_learned": row[6],
-            }
-            data.append(x)
-            counter += 1
-    return render(request, "leagues.html", {"data": data})
+    return render_league("Gamma", request)
 
 
 def delta(request):
-    data = []
-    counter = 1
-    conn = sqlite3.connect(database_path)
-    c = conn.cursor()
-    c.execute(
-        "SELECT username, xp, time_spend, reviews, retention, league, days_learned FROM League WHERE suspended IS NULL ORDER BY xp DESC"
-    )
-
-    for row in c.fetchall():
-        if row[5] == "Delta" and row[1] != 0:
-            x = {
-                "place": counter,
-                "username": row[0],
-                "xp": row[1],
-                "time": row[2],
-                "reviews": row[3],
-                "retention": row[4],
-                "days_learned": row[6],
-            }
-            data.append(x)
-            counter += 1
-    return render(request, "leagues.html", {"data": data})
+    return render_league("Delta", request)
 
 
 def privacy(request):
