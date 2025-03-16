1fb4471fe2eb2764e9a408db9b4a5dca9b3f0029
Chris Stelzmüller <tuesd4y@protonmail.ch>
2025-03-16 16:01:11 +0100

    Feature/deploy server (#2)
    * Create server config and requirements for docker deploy
    
    * Add docker config
    
    * Add workflow for automatically building container image
    
    * Update workflow

--- manage.py
+++ manage.py
@@ -7,7 +7,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "server.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "server.local")
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
